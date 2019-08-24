8cca769c253b8e6a527de83930eba9b4accf470e
Alexander <alexandernino98@gmail.com>
2019-08-24 18:00:23 +0200

    Updates

---
+++ precache-manifest.96ad054bde1b65c7724d526c8fb6f137.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a2b4b9355c266a4d4c10c7145ebb61",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5a681280f35325f42732",
+    "url": "/weather-app/static/css/main.cc476d96.chunk.css"
+  },
+  {
+    "revision": "510c4095839e3650da04",
+    "url": "/weather-app/static/js/2.060100d8.chunk.js"
+  },
+  {
+    "revision": "5a681280f35325f42732",
+    "url": "/weather-app/static/js/main.6897b7e2.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  },
+  {
+    "revision": "e323ecf6f369aeb7be01c7880c385af8",
+    "url": "/weather-app/static/media/bg.e323ecf6.jpg"
+  },
+  {
+    "revision": "4dc8014edb976cc56a8c463f62522509",
+    "url": "/weather-app/static/media/cloudy.4dc8014e.png"
+  },
+  {
+    "revision": "afed36278400d3f0900ee004b8b7ee74",
+    "url": "/weather-app/static/media/rain.afed3627.png"
+  },
+  {
+    "revision": "bcb3306a08b8a1c94207d7b0c2113acf",
+    "url": "/weather-app/static/media/sunny.bcb3306a.png"
+  }
+]);
